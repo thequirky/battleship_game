3a3acf4600bf38f6a3a8caaaf53bb169281d3065
evan <chatziva@protonmail.com>
2023-04-25 23:23:44 +0100

    refactor and deindent

--- board.py
+++ board.py
@@ -1,14 +1,36 @@
 from enum import Enum
 
 
-class Cell(Enum):
+class CellState(Enum):
     HIDDEN = "H"
-    MISS = "O"
+    MISSED = "O"
     HIT = "X"
 
 
-class Ship:
+class Position:
+
+    def __init__(self, x=0, y=0) -> None:
+        self.x = x
+        self.y = y
+
+
+class ShipBuilder:
     ...
+
+
+class Ship:
+
+    def __init__(self, start: Position, end: Position, damaged: list[Position] = None) -> None:
+        self.start = start
+        self.end = end
+        if damaged is None:
+            self._damaged = []
+
+    def do_hit(self, position: Position) -> bool:
+        ...
+
+    def is_sunk(self):
+        ...
 
 
 class Board:
@@ -18,4 +40,10 @@ class Board:
 
     @classmethod
     def from_ship(cls):
+        ...
+
+    def place_ship(self):
+        ...
+
+    def display(self) -> None:
         ...
--- test.py
+++ test.py
@@ -1,9 +1,25 @@
+from enum import Enum, auto
 import random
 
+
+class Orientation(Enum):
+    HORIZONTAL = auto()
+    VERTICAL = auto()
+
+
+SHIP_TYPE_TO_SIZE = {
+    "Carrier": 5,
+    "Battleship": 4,
+    "Cruiser": 3,
+    "Submarine": 3,
+    "Destroyer": 2,
+}
+
+
 class Ship:
-    def __init__(self, name, size):
-        self.name = name
-        self.size = size
+    def __init__(self, type: str):
+        self.name = type
+        self.size = SHIP_TYPE_TO_SIZE[type]
         self.coords = []
         self.hits = []
 
@@ -11,9 +27,9 @@ class Ship:
         while True:
             x = random.randint(0, 9)
             y = random.randint(0, 9)
-            orientation = random.choice(['horizontal', 'vertical'])
+            orientation = random.choice(list(Orientation))
             if self.check_valid_placement(board, x, y, orientation):
-                if orientation == 'horizontal':
+                if orientation == Orientation.HORIZONTAL:
                     for i in range(self.size):
                         board[x][y+i] = self.name[0]
                         self.coords.append((x, y+i))
@@ -23,8 +39,8 @@ class Ship:
                         self.coords.append((x+i, y))
                 break
 
-    def check_valid_placement(self, board, x, y, orientation):
-        if orientation == 'horizontal':
+    def check_valid_placement(self, board, x, y, orientation: Orientation):
+        if orientation == Orientation.HORIZONTAL:
             if y + self.size > 10:
                 return False
             for i in range(self.size):
@@ -41,18 +57,12 @@ class Ship:
     def is_sunk(self):
         return len(self.hits) == self.size
 
-class Board:
-    def __init__(self):
-        self.board = [['-' for i in range(10)] for j in range(10)]
-        self.ships = []
-        self.create_ships()
 
-    def create_ships(self):
-        self.ships.append(Ship('Carrier', 5))
-        self.ships.append(Ship('Battleship', 4))
-        self.ships.append(Ship('Cruiser', 3))
-        self.ships.append(Ship('Submarine', 3))
-        self.ships.append(Ship('Destroyer', 2))
+class Board:
+    def __init__(self, size: int = 10):
+        self.size = size
+        self.board = [['-' for i in range(10)] for j in range(10)]
+        self.ships = [Ship(type) for type in SHIP_TYPE_TO_SIZE.keys()]
 
     def print_board(self, show_ships=False):
         print('   0 1 2 3 4 5 6 7 8 9')
@@ -85,21 +95,23 @@ class Board:
                 return guess
 
     def process_guess(self, guess):
-        row, col = guess
-        if self.board[row][col] != '-':
-            for ship in self.ships:
-                if guess in ship.coords:
-                    ship.hits.append(guess)
-                    self.board[row][col] = 'X'
-                    print('Hit!')
-                    if ship.is_sunk():
-                        print(f'{ship.name} has been sunk!')
-        else:
+        x, y = guess
+
+        if self.board[x][y] == '-':
             print('Miss!')
-            self.board[row][col] = 'O'
+            self.board[x][y] = 'O'
+            return
+
+        for ship in self.ships:
+            if guess in ship.coords:
+                ship.hits.append(guess)
+                self.board[x][y] = 'X'
+                print('Hit!')
+                if ship.is_sunk():
+                    print(f'{ship.name} has been sunk!')
 
 
-if __name__ == '__main__':
+def main():
     board = Board()
     board.place_all_ships()
     board.print_board(show_ships=False)
@@ -114,3 +126,7 @@ if __name__ == '__main__':
         if all(ship.is_sunk() for ship in board.ships):
             print('Congratulations! You have sunk all the ships!')
             break
+
+
+if __name__ == '__main__':
+    main()

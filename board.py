60553dec33653cd1440793afff231df8279fd586
evan <chatziva@protonmail.com>
2023-04-29 11:56:00 +0100

    simplify

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-from cell import CellState, Position
+from cell import CellState as cs, Position
 
 
 BOARD_SIZE = 10
@@ -10,28 +10,28 @@ GAP = 2 * SPACE
 class Board:
     def __init__(self, size: int = BOARD_SIZE):
         self.size = size
-        self.grid = [[CellState.EMPTY.value for _ in range(size)] for _ in range(size)]
+        self.grid = [[cs.EMPTY.value for _ in range(size)] for _ in range(size)]
 
-    def get_value(self, pos: tuple[str, str]) -> str:
-        x, y = pos
-        return self.grid[x][y]
+    def get_value(self, pos: Position) -> str:
+        return self.grid[pos.x][pos.y]
     
-    def set_value(self, pos: tuple[str, str], value: str) -> None:
-        x, y = pos
-        self.grid[x][y] = value
+    def set_value(self, pos: Position, value: str) -> None:
+        self.grid[pos.x][pos.y] = value
 
     def is_empty(self, guess: Position) -> bool:
-        return self.get_value(guess) == CellState.EMPTY.value
+        return self.get_value(guess) == cs.EMPTY.value
 
     def __str__(self):
         brd_str = GAP + SPACE + SPACE.join(str(idx) for idx in range(self.size))
-        for i in range(self.size):
-            row = str(i) + GAP
-            for j in range(self.size):
-                if (self.grid[i][j] == CellState.EMPTY.value or 
-                    self.grid[i][j] in [CellState.HIT.value, CellState.MISS.value]):
-                    row += self.grid[i][j] + SPACE
+        for x in range(self.size):
+            row = str(x) + GAP
+            for y in range(self.size):
+                pos = Position(x,y)
+                cell_value = self.get_value(pos)
+                if (cell_value == cs.EMPTY.value or 
+                    cell_value in [cs.HIT.value, cs.MISS.value]):
+                    row += cell_value + SPACE
                 else:
-                    row += CellState.EMPTY.value + SPACE
+                    row += cs.EMPTY.value + SPACE
             brd_str += "\n" + row
         return brd_str
--- game.py
+++ game.py
@@ -1,21 +1,21 @@
 import random
 from board import BOARD_SIZE, Board
-from cell import CellState, Position
+from cell import CellState as cs, Position
 from ship import Orientation, Ship, SHIP_TYPES
 
 
 class Game:
     def __init__(self) -> None:
-        self.guessed_coords = []
+        self.guessed_coords : list [Position] = []
         self.board = Board()
         self.ships = [Ship(type) for type in SHIP_TYPES]
         self.place_all_ships()
 
-    def place_all_ships(self):
+    def place_all_ships(self) -> None:
         for ship in self.ships:
             self.place_ship(ship)
 
-    def place_ship(self, ship: Ship):
+    def place_ship(self, ship: Ship) -> None:
         while True:
             pos = Position(random.randint(0, 9), random.randint(0, 9))
             orientation = random.choice(list(Orientation))
@@ -27,11 +27,11 @@ class Game:
                     ship.place_vertically(pos, self.board)
                 break
 
-    def get_valid_guess(self):
+    def get_valid_guess(self) -> Position:
         while True:
             guess = input('Enter your guess (row, column): ')
             guess = guess.split(',')
-            guess = (int(guess[0]), int(guess[1]))
+            guess = Position(int(guess[0]), int(guess[1]))
             if guess in self.guessed_coords:
                 print('You already guessed that, try again')
             elif guess[0] not in range(BOARD_SIZE) or guess[1] not in range(BOARD_SIZE):
@@ -41,13 +41,13 @@ class Game:
 
     def process_guess(self, guess) -> str:
         if self.board.is_empty(guess):
-            self.board.set_value(guess, CellState.MISS.value)
+            self.board.set_value(guess, cs.MISS.value)
             return "Miss!"
 
         for ship in self.ships:
             if guess in ship.coords:
                 ship.hits.append(guess)
-                self.board.set_value(guess, CellState.HIT.value)
+                self.board.set_value(guess, cs.HIT.value)
                 msg = 'Hit!\n'
                 if ship.is_sunk():
                     msg += f'{ship.name} has been sunk!'

ab2b2513da561efb7a7daff5a52604ad0545732d
dingo <chatziva@protonmail.com>
2023-05-07 23:14:56 +0100

    simplifications

--- ship.py
+++ ship.py
@@ -43,27 +43,16 @@ class Ship:
             board.set_value(p, self.type.value)
             self.coords.append(p)
 
-    def can_place_vertically(self, board: Board, pos: Position) -> bool:
-        can_fit = pos.x + self.size < board.size
-        if not can_fit:
-            return False
-        positions = [Position(pos.x + i, pos.y) for i in range(self.size)]
-        no_obstacle = all(board.get_value(pos) == Cell.EMPTY for pos in positions)
-        return no_obstacle
-
-    def can_place_horizontally(self, board: Board, pos: Position) -> bool:
-        can_fit = pos.y + self.size < board.size
-        if not can_fit:
-            return False
-        positions = [Position(pos.x, pos.y + i) for i in range(self.size)]
-        no_obstacle = all(board.get_value(pos) == Cell.EMPTY for pos in positions)
-        return no_obstacle
-
     def can_place(self, board: Board, pos: Position, orientation: Orientation) -> bool:
-        if orientation == Orientation.HORIZONTAL:
-            return self.can_place_horizontally(board, pos)
+        can_fit = (pos.x + self.size < board.size) or (pos.y + self.size < board.size)
+        if not can_fit:
+            return False
+        if orientation == Orientation.VERTICAL:
+            positions = [Position(pos.x + i, pos.y) for i in range(self.size)]
         else:
-            return self.can_place_vertically(board, pos)
+            positions = [Position(pos.x, pos.y + i) for i in range(self.size)]
+        has_no_obstacles = all(board.get_value(pos) == Cell.EMPTY for pos in positions)
+        return has_no_obstacles
 
     def is_sunk(self):
         return len(self.hits) == self.size

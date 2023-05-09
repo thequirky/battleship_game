2674dd4b04f48cb452abe86d5ce6473789983043
dingo <chatziva@protonmail.com>
2023-05-09 21:42:41 +0100

    formatting

--- placement.py
+++ placement.py
@@ -6,11 +6,11 @@ from ship import Ship
 
 
 def place_ship_on_position(
-        ship: Ship, 
-        pos: Position, 
-        board: Board, 
-        orientation: Orientation
-    ) -> None:
+    ship: Ship,
+    pos: Position,
+    board: Board,
+    orientation: Orientation
+) -> None:
     if orientation == Orientation.HORIZONTAL:
         positions = [Position(pos.x, pos.y + i) for i in range(ship.size)]
     else:
@@ -19,12 +19,13 @@ def place_ship_on_position(
         board.set_value(p, ship.type.value)
         ship.coords.append(p)
 
+
 def can_place_ship_on_position(
-        ship: Ship, 
-        board: Board, 
-        pos: Position, 
-        orientation: Orientation
-    ) -> bool:
+    ship: Ship,
+    board: Board,
+    pos: Position,
+    orientation: Orientation
+) -> bool:
     can_fit_vertically = pos.x + ship.size < board.size
     can_fit_horizontally = pos.y + ship.size < board.size
     if orientation == Orientation.VERTICAL and can_fit_vertically:
@@ -33,13 +34,29 @@ def can_place_ship_on_position(
         positions = [Position(pos.x, pos.y + i) for i in range(ship.size)]
     else:
         return False
-    has_no_obstacles = all(board.get_value(pos) == Cell.EMPTY for pos in positions)
+    has_no_obstacles = all(
+        board.get_value(pos) == Cell.EMPTY for pos in positions
+    )
     return has_no_obstacles
+
 
 def place_ship_randomly(ship: Ship, board: Board) -> None:
     while True:
-        pos = Position(random.randint(0, board.size - 1), random.randint(0, board.size - 1))
+        pos = Position(
+            x=random.randint(0, board.size - 1),
+            y=random.randint(0, board.size - 1)
+        )
         orientation = random.choice(list(Orientation))
-        if can_place_ship_on_position(ship=ship, board=board, pos=pos, orientation=orientation):
-            place_ship_on_position(ship=ship, pos=pos, board=board, orientation=orientation)
+        if can_place_ship_on_position(
+            ship=ship, 
+            board=board, 
+            pos=pos, 
+            orientation=orientation
+        ):
+            place_ship_on_position(
+                ship=ship, 
+                pos=pos, 
+                board=board, 
+                orientation=orientation
+            )
             break
--- test_game.py
+++ test_game.py
@@ -1,20 +1,37 @@
 from board import Board
 from game import Game
-from position import Position
-from ship import Ship
+from position import Position, Orientation
+from ship import Ship, ShipType
+from placement import place_ship_on_position
 
 
 def main():
-    from ship import ShipType
-    from placement import place_ship_on_position
-    from position import Orientation
 
     game = Game()
     game.board = Board()
-    game.ships = [Ship(ShipType.Carrier), Ship(ShipType.Cruiser), Ship(ShipType.Battleship)]
-    place_ship_on_position(game.ships[0], Position(x=0, y=0), game.board, orientation=Orientation.HORIZONTAL)
-    place_ship_on_position(game.ships[1], Position(x=0, y=5), game.board, orientation=Orientation.VERTICAL)
-    place_ship_on_position(game.ships[2], Position(x=0, y=6), game.board, orientation=Orientation.HORIZONTAL)
+    game.ships = [
+        Ship(ShipType.Carrier),
+        Ship(ShipType.Cruiser),
+        Ship(ShipType.Battleship)
+    ]
+    place_ship_on_position(
+        ship=game.ships[0],
+        pos=Position(x=0, y=0),
+        board=game.board,
+        orientation=Orientation.HORIZONTAL
+    )
+    place_ship_on_position(
+        ship=game.ships[1],
+        pos=Position(x=0, y=5),
+        board=game.board,
+        orientation=Orientation.VERTICAL
+    )
+    place_ship_on_position(
+        game.ships[2],
+        pos=Position(x=0, y=6),
+        board=game.board,
+        orientation=Orientation.HORIZONTAL
+    )
     for row in range(game.board.size):
         print(game.board.grid[row])
     game.run()

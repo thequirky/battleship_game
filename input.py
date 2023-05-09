8dab513a29c9d5b59aed1710617834a476574144
dingo <chatziva@protonmail.com>
2023-05-09 21:59:38 +0100

    input validation

--- input.py
+++ input.py
@@ -1,11 +1,20 @@
 from position import Position
 
 
+def get_number(prompt: str) -> int:
+    while True:
+        value = input(prompt)
+        try:
+            value = int(value)
+            return value
+        except ValueError:
+            print("Not a number, try again")
+
+
 def get_valid_guess(board_size: int, already_guessed: list[Position]) -> Position:
     while True:
-        x = input('Enter your row guess: ')
-        y = input('Enter your column guess: ')
-        x, y = int(x), int(y)
+        x = get_number('Enter your row guess: ')
+        y = get_number('Enter your column guess: ')
         guess = Position(x, y)
         if guess in already_guessed:
             print('You already guessed that, try again')
